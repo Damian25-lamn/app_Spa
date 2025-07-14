bd4a0a5fbf776949247b1da19b9ec38d02a905d7
Damian <adminda@uce.edu.ec>
2025-07-14 15:30:00 -0500

    Subida inicial del proyecto

---
+++ clientes/src/test/java/com/equipo/sistema_citas/SistemaCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo.sistema_citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
